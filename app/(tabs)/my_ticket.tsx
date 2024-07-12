3e3e8d5a9546e85f56e7ed2a0da1d3547d2801e5
Sithum Imesh <sithum.imesh052002@icloud.com>
2024-07-12 09:28:09 +0530

    My Ticket header and Button

--- app/(tabs)/my_ticket.tsx
+++ app/(tabs)/my_ticket.tsx
@@ -1,11 +1,42 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { Stack, useRouter } from 'expo-router'
+import { Feather } from '@expo/vector-icons'
+import { AntDesign } from '@expo/vector-icons';
+import Colors from '@/constants/Colors'
+import MyTicketButton from '@/component/MyTicketButton/MyTicketButton';
 
 const MyTicket = () => {
+  const router = useRouter();
   return (
-    <View className='flex-1 justify-center items-center bg-white'>
-      <Text>My Ticket</Text>
-    </View>
+    <>
+   <Stack.Screen
+        options={{
+          headerTitle: "My Tickets",
+          headerTitleStyle: {
+            fontSize: 25,
+            fontWeight: 'bold',
+          },
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <View className='mr-2 ml-4 bg-black p-1 rounded-lg'>
+                <Feather name='arrow-left' size={28} color={Colors.white} />
+              </View>
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity onPress={() => {}}>
+              <View className='mr-3 p-1 rounded-lg'>
+              <AntDesign name="calendar" size={28}  color="black" />
+              </View>
+            </TouchableOpacity>
+          )
+        }}
+      />
+      <View>
+        <MyTicketButton />
+      </View>
+    </>
   )
 }
 
