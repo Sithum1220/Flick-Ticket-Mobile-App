f2f757f746971abc97973de438970960ca5fb873
Sithum Imesh <sithum.imesh052002@icloud.com>
2024-07-23 01:06:39 +0530

    Profile Screen done

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -5,14 +5,16 @@ import {
   ScrollView,
   Text,
   TouchableOpacity,
+  Switch,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import { Kanit_400Regular } from "@expo-google-fonts/kanit";
 import { Colors } from "@/constants/Colors";
-import { Feather, FontAwesome, Fontisto, Ionicons } from "@expo/vector-icons";
+import { Feather, FontAwesome, Fontisto, Ionicons, SimpleLineIcons } from "@expo/vector-icons";
 import ProfileSettingOption from "@/components/ProfileSettingOption/ProfileSettingOption";
+import { Icons } from "@/components/Icons/Icons";
 
 export default function Profile() {
   let [fontsLoaded] = useFonts({
@@ -22,6 +24,8 @@ export default function Profile() {
   if (!fontsLoaded) {
     return null;
   }
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
   return (
     <>
       <Stack.Screen
@@ -51,7 +55,7 @@ export default function Profile() {
               />
             </View>
 
-            <View className="flex-row items-center justify-between pt-[5%]">
+            <View className="flex-row items-center pr-2 justify-between pt-[5%]">
               <View className="p-4 flex-col gap-1">
                 <Text
                   style={{ fontFamily: "Kanit_400Regular" }}
@@ -75,27 +79,50 @@ export default function Profile() {
               </View>
             </View>
 
-            <View className="p-4 mt-[6%] flex-col gap-8">
+            <View className="p-4 scroll-pr-40 mt-[6%] flex-col gap-8">
               <ProfileSettingOption
                 name="edit"
                 IconComponent={Feather}
                 title={"Edit Profile"}
+                onPress={() => {}}
+                showArrowIcon={true}
+                ArrowIconComponent={FontAwesome}
               />
               <ProfileSettingOption
                 name="moon-o"
                 IconComponent={FontAwesome}
                 title={"Dark Mode"}
+                onPress={() => {}}
+                showArrowIcon={false}
+                switchValue={isEnabled}
+                onSwitchChange={toggleSwitch}
               />
               <ProfileSettingOption
                 name="phone"
                 IconComponent={Feather}
                 title={"Help & Support"}
+                onPress={() => {}}
+                showArrowIcon={true}
+                ArrowIconComponent={FontAwesome}
               />
               <ProfileSettingOption
                 name="world-o"
                 IconComponent={Fontisto}
                 title={"Edit Profile"}
+                onPress={() => {}}
+                showArrowIcon={true}
+                ArrowIconComponent={FontAwesome}
               />
+            </View>
+
+            <View className="justify-center items-center pt-[8%]">
+            <TouchableOpacity>
+            <View className="flex-row gap-5 items-center">
+                <Icons IconComponent={SimpleLineIcons} name={'logout'} size={24} color={Colors.primaryColor}/>
+                <Text className="text-red-400 text-xl font-semibold">Logout</Text>
+              </View>
+            </TouchableOpacity>
+             
             </View>
           </View>
         </ScrollView>
--- components/ProfileSettingOption/ProfileSettingOption.tsx
+++ components/ProfileSettingOption/ProfileSettingOption.tsx
@@ -1,17 +1,37 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { FontAwesome } from "@expo/vector-icons";
-import { IconProps } from "@expo/vector-icons/build/createIconSet";
-
+import { View, Text, TouchableOpacity, Switch } from "react-native";
 import { Icons } from "../Icons/Icons";
 import { Colors } from "@/constants/Colors";
 
-
-export default function ProfileSettingOption(props:any) {
+export default function ProfileSettingOption(props: any) {
   return (
-    <View className="flex-row items-center gap-4">
-      <Icons size={28} IconComponent={props.IconComponent} name={props.name} color={Colors.text.gray} />
-      <Text>{props.title}</Text>
-    </View>
+    <TouchableOpacity onPress={props.onPress}>
+      <View className="justify-between flex-row items-center">
+        <View className="flex-row items-center gap-4">
+          <Icons
+            size={28}
+            IconComponent={props.IconComponent}
+            name={props.name}
+            color={Colors.text.gray}
+          />
+          <Text>{props.title}</Text>
+        </View>
+        <View>
+          {props.showArrowIcon ? (
+            <Icons
+              size={20}
+              IconComponent={props.ArrowIconComponent}
+              name={'caret-right'}
+              color={Colors.text.gray}
+            />
+          ) : (
+            <Switch
+              value={props.switchValue}
+              trackColor={{false: '#767577', true: ''}}
+              onValueChange={props.onSwitchChange}
+            />
+          )}
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 }
